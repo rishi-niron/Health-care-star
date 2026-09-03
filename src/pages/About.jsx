0be10b21920cdf91e93624e3554779c4afc4c1f2
Rishiattri <rishi@Nitontech.com>
2026-09-03 17:05:45 +0530

    new  updated for testing perpus

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section>
+      <h1>About</h1>
+      <p>This is the about page for Health-care Star.</p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <section>
+      <h1>Home</h1>
+      <p>Welcome to Health-care Star.</p>
+    </section>
+  )
+}
+
+export default Home
