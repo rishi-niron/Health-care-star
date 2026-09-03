d52b1ba859d198086afc11f84f0df452b8de7483
Rishiattri <rishi@Nitontech.com>
2026-09-03 17:13:18 +0530

    update

--- src/broken.js
+++ src/broken.js
@@ -1,9 +1,5 @@
 function transferMoney(sender, receiver, amount) {
-<<<<<<< HEAD
   if (amount <= 0) {
-=======
-  if (amount === 0) {
->>>>>>> 92074e10a514ea993f5e739c0a38aee2c9364c03
     return false;
   }
 
